7a405931810bfab04ddb6683db33e930815fe6d5
Allred <andrewrallred@gmail.com>
2020-10-11 23:35:02 -0400

    updating some comments

--- quantizers/microtonal-quantizer.lua
+++ quantizers/microtonal-quantizer.lua
@@ -1,11 +1,8 @@
 --- microtonal quantizer example
 -- andrew allred 2020.10.08
--- in1: clock
--- in2: voltage to quantize
--- out1: in2 quantized to scale1 on clock pulses
--- out2: in2 quantized to scale2 on clock pulses
--- out3: in2 quantized to scale3 continuously
--- out4: trigger pulses when out3 changes
+-- in1: voltage to quantize
+-- in2: sets notes per octave from 7 to 57
+-- out1: in1 quantized in real time
 
 function quantize(volts,notesPerOctave) 
     volts = math.abs(volts)
